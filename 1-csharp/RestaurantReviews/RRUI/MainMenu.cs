ae89de0dd440aa294a763bb3f25b2aac5b1a8d04
Jean Monegro <jeanmonegro1@gmail.com>
2021-05-04 16:51:12 -0700

    5/4

--- 1-csharp/RestaurantReviews/RRUI/MainMenu.cs
+++ 1-csharp/RestaurantReviews/RRUI/MainMenu.cs
@@ -1,39 +1,61 @@
+using System;
+using RRModels;
+using System.Collections.Generic;
 namespace RRUI
 {
-    public class MainMenu
+    public class MainMenu : IMenu
     {
-        public void Start(){
-
+        public void Start()
+        {
             bool repeat = true;
-            do{
-            Console.WriteLine("Welcome to my Restaurant Reviews Application!");
-            Console.WriteLine("What would you like to do?");
-            Console.WriteLine("[0] add a restaurant");
-            console.WriteLine("[1] Exit");
-            string input = Console.Readline();
-            switch(input)
+            do
             {
-                case "0":
-                    ViewRestaurant();
-                    break;
-                case "1":
-
-                    break;
-                default:
-                    Console.WriteLine("Please input a valid option");
-
-            }
+                Console.WriteLine("Welcome to my Restaurant Reviews Application!");
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("[0] View a restaurant");
+                Console.WriteLine("[1] Exit");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        //add a restaurant
+                        ViewRestaurant();
+                        break;
+                    case "1":
+                        //exit
+                        Console.WriteLine("Goodbye cruel world.");
+                        repeat = false;
+                        break;
+                    default:
+                        //invalid input
+                        Console.WriteLine("Please input a valid option");
+                        break;
+                }
             } while (repeat);
+
+
         }
+
         private void ViewRestaurant()
         {
+            //TODO: Remove the hardcoded restaurant and refer to a stored restaurant that exists
             Restaurant goodTaste = new Restaurant("Good Taste", "Baguio City", "Benguet");
-            goodTaste.Review = new Review
+            goodTaste.Reviews = new List<Review>
             {
-                Rating = 5,
-                Description = "A M A Z I N G"
+                new Review{
+                    Rating = 5,
+                    Description = "A M A Z I N G"
+                },
+                new Review{
+                    Rating = 5,
+                    Description = "Good food for cheap."
+                }
             };
             Console.WriteLine(goodTaste.ToString());
+            foreach (Review review in goodTaste.Reviews)
+            {
+                Console.WriteLine(review.ToString());
+            }
         }
     }
 }
--- 1-csharp/RestaurantReviews/RRUI/Program.cs
+++ 1-csharp/RestaurantReviews/RRUI/Program.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using RRModels;
-namespace RRUI
+﻿  namespace RRUI
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Setting a parent type to an instance of a subtype is called covariance
             IMenu menu = new MainMenu();
             menu.Start();
-
         }
     }
 }
