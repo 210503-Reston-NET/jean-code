1eddd56e6064ef276b9b544f9f0bf83b499ce485
Jean Monegro <jeanmonegro1@gmail.com>
2021-05-08 14:22:21 -0700

    5/8

--- delegate/Program.cs
+++ delegate/Program.cs
@@ -1,34 +1,24 @@
 ﻿using System;
-
-delegate int NumberChanger(int n);
-
-namespace Delegate
+class Program
 {
-    class TestDelegate {
-    static int num = 10;
-    public static int AddNum(int p) {
-        num += p;
-        return num;
-    }
-    public static int MultNum(int q) {
-         num *= q;
-        return num;
-    }
-    public static int getNum() {
-    return num;
-    }
-    static void Main(string[] args) {
-         //create delegate instances
-        NumberChanger nc1 = new NumberChanger(AddNum);
-        NumberChanger nc2 = new NumberChanger(MultNum);
-         //calling the methods using the delegate objects
-        nc1(25);
-        Console.WriteLine("Value of Num: {0}", getNum());
-        nc2(5);
-        Console.WriteLine("Value of Num: {0}", getNum());
-        Console.ReadKey();
+    public delegate string NewDel(string str);
+
+    class EventProgram{
+        event NewDel MyEvent;
+
+        public EventProgram(){
+            this.MyEvent += new NewDel(this.WelcomeUser);
+        }
+
+        public string WelcomeUser(string username){
+            return "Welcome " + username;
+        }
+        static void Main(string[] args)
+        {
+            EventProgram obj1 = new EventProgram();
+            string result = obj1.MyEvent("Tutorials Point");
+            Console.WriteLine(result);
+            Console.WriteLine("Hi");
         }
     }
 }
-
-
