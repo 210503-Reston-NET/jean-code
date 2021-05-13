e72aa62d137a46e9d99146cfbc982a9d28df7d28
Jean Monegro <jeanmonegro1@gmail.com>
2021-05-13 06:32:22 -0700

    5/13

---
+++ 1-csharp/Practice/StoreModels/Item.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace StoreModels
+{
+    public class Item
+    {
+        public string BikeBrand{get;set;}
+
+        public string BikeType{get;set;}
+
+        // public List<Item> Items {get;set;}
+
+        public override string ToString()
+        {
+            return $" BikeType: {BikeBrand} \n\n TireType: {BikeType}";
+        }
+    }
+}
---
+++ 1-csharp/Practice/StoreUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using StoreModels;
+
+namespace StoreUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            menu X = new menu();
+            X.StartMenu();
+        }
+    }
+}
